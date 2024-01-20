e609ff43f21bdbb01133e9d709fc5373ef3569f3
francois <francoisberi76@gmail.com>
2024-01-20 17:23:24 +0100

    code fonctionnel box plot vertical question 3

--- projet.py
+++ projet.py
@@ -8,13 +8,13 @@ df = pd.read_csv('dataset.txt', sep='\t')
 num_columns = df.shape[1]
 
 # Create a figure with a subplot for each column
-fig, axes = plt.subplots(nrows=num_columns, ncols=1, figsize=(10, num_columns * 5))
+fig, axes = plt.subplots(nrows=1, ncols=num_columns, figsize=(num_columns * 6, 6))
 
 # Create individual box plots for each column
 for i, column in enumerate(df.columns):
-    df.boxplot(column=column, ax=axes[i])
+    df.boxplot(column=column, ax=axes[i], vert=False)
     axes[i].set_title(f'Box Plot of {column}')
-    axes[i].set_ylabel('Values')
+    axes[i].set_xlabel('Values')
 
 plt.tight_layout()
 plt.show()
