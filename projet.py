135e2b17fcc5ed378dd397b35b1d74308d39c5d2
vaylonn <114398794+vaylonn@users.noreply.github.com>
2024-01-23 00:04:48 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -12,17 +12,17 @@ df = pd.read_csv('dataset.txt', sep='\t')
 # Determine the number of columns in the DataFrame
 num_columns = df.shape[1]
     
-# # Create a figure with a subplot for each column
-# fig, axes = plt.subplots(nrows=1, ncols=num_columns, figsize=(num_columns * 6, 6))
+# Create a figure with a subplot for each column
+fig, axes = plt.subplots(nrows=1, ncols=num_columns, figsize=(num_columns * 6, 6))
 
-# # Create individual box plots for each column
-# for i, column in enumerate(df.columns):
-#     df.boxplot(column=column, ax=axes[i], vert=False)
-#     axes[i].set_title(f'Box Plot of {column}')
-#     axes[i].set_xlabel('Values')
+# Create individual box plots for each column
+for i, column in enumerate(df.columns):
+    df.boxplot(column=column, ax=axes[i], vert=False)
+    axes[i].set_title(f'Box Plot of {column}')
+    axes[i].set_xlabel('Values')
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
 
 transformer = preprocessing.StandardScaler().fit(df)
 df_transformed = transformer.transform(df)
