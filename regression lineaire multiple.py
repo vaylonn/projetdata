d5546ce377fa584c4cb3e45cb055449f1132d600
Ethan-eht <105300067+Ethan-eht@users.noreply.github.com>
2024-01-23 00:13:47 +0100

    Update regression lineaire multiple.py

--- regression lineaire multiple.py
+++ regression lineaire multiple.py
@@ -1,46 +1,47 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from itertools import combinations
+import itertools
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
-# Load the dataset
-df = pd.read_csv('CCPP_data.txt', delimiter='\t')
+# Load the data
+data = pd.read_csv('CCPP_data.txt', sep='\t')
 
-# Define the target variable and the feature variables
-X = df[['AT', 'V', 'AP', 'RH']]
-Y = df['EP']
+# Define the target variable and predictors
+y = data['EP']
+X = data.drop('EP', axis=1)
 
-# Define the maximum number of features to consider
-max_features = 4
+# Store the results
+results = []
 
-# Define the list of feature combinations to consider
-feature_combinations = []
-for i in range(1, max_features + 1):
-    feature_combinations += list(combinations(X.columns, i))
+# Iterate over all possible combinations of predictors (from 1 to 4)
+for k in range(1, 5):
+    for variables in itertools.combinations(X.columns, k):
+        # Fit the model
+        X_subset = sm.add_constant(X[list(variables)])
+        model = sm.OLS(y, X_subset)
+        results.append({
+            'variables': variables,
+            'model': model.fit()
+        })
 
-# Define the list of adjusted R-squared values for each feature combination
-adj_r2_values = []
-for features in feature_combinations:
-    reg = LinearRegression().fit(X[list(features)], Y)
-    adj_r2_values.append(1 - (1 - reg.score(X[list(features)], Y)) * ((len(Y) - 1) / (len(Y) - len(features) - 1)))
+# Find the best model (the one with the highest adjusted R-squared)
+best_model = max(results, key=lambda item: item['model'].rsquared_adj)
 
-# Plot the adjusted R-squared values versus the number of features
-plt.plot(range(1, len(adj_r2_values) + 1), adj_r2_values)
+# Print the best model's details
+print('Best model variables:', best_model['variables'])
+print('Best model adjusted R-squared:', best_model['model'].rsquared_adj)
+print('Best model coefficients:', best_model['model'].params)
+
+# Plot the adjusted R-squared versus the number of features
+plt.plot([len(item['variables']) for item in results], [item['model'].rsquared_adj for item in results], 'ro-')
 plt.xlabel('Number of features')
 plt.ylabel('Adjusted R-squared')
 plt.show()
 
-# Select the best feature combination
-best_features = feature_combinations[np.argmax(adj_r2_values)]
-print(f"The best feature combination is: {best_features}\n")
+# Perform the zero slope hypothesis test for all the coefficients except β0
+print('Zero slope hypothesis test results:')
+for variable in best_model['variables']:
+    if variable in best_model['model'].params.index:
+        print(f'{variable}:', best_model['model'].pvalues[variable])
 
-# Fit the linear regression model using the best feature combination
-reg = LinearRegression().fit(X[list(best_features)], Y)
-
-# Print the coefficient estimates and the coefficient of determination R^2
-beta_0 = reg.intercept_
-beta_i = reg.coef_
-R2 = reg.score(X[list(best_features)], Y)
-print(f"The coefficient estimates are: beta_0 = {beta_0:.2f}, beta_i = {beta_i}")
-print(f"The coefficient of determination R^2 is: {R2:.4f}")
